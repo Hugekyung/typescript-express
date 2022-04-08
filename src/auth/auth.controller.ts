f792dc579bfa7ddb8d696307b052358075f68f6f
haechan <kiki9510@gmail.com>
2022-04-08 17:57:26 +0900

    Added signin api and request dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import { v4 as uuidV4 } from "uuid";
 import userMemory from "./auth.model";
+import { userRequest } from "./dto/request.dto";
 
 const AuthRouter = Router();
 
@@ -14,8 +15,30 @@ AuthRouter.post("/signup", (req: Request, res: Response) => {
             username,
             password,
         });
+
+        res.status(201).json({ message: "user signup successfully! " });
     } catch (error) {
         throw new Error("Server Error");
+    }
+});
+
+AuthRouter.post("/signin", (req: userRequest, res: Response) => {
+    try {
+        console.log("now userMemory>> ", userMemory);
+        const { username, password } = req.body;
+
+        const user = userMemory.find((user) => user.username === username);
+        if (user && user.password === password) {
+            req.user = user;
+        }
+
+        console.log("login user >>", req.user);
+
+        res.status(200).json({
+            message: `user login successfully ! hello ${username}`,
+        });
+    } catch (error) {
+        throw new Error("Error");
     }
 });
 
