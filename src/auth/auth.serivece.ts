6e5addf37a803bb775d47f370fcdd39fb473821c
haechan <kiki9510@gmail.com>
2022-04-08 18:39:42 +0900

    Added constructor in auth service class

--- src/auth/auth.serivece.ts
+++ src/auth/auth.serivece.ts
@@ -1,7 +1,33 @@
 import UserMemory from "./auth.model";
 
 export class AuthService {
-    static createUser({ userId, username, password }) {
+    userId: string;
+    username: string;
+    password: string;
+
+    constructor({
+        userId,
+        username,
+        password,
+    }: {
+        userId: string;
+        username: string;
+        password: string;
+    }) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+    }
+
+    static createUser({
+        userId,
+        username,
+        password,
+    }: {
+        userId: string;
+        username: string;
+        password: string;
+    }) {
         UserMemory.push({
             id: userId,
             username,
@@ -11,7 +37,7 @@ export class AuthService {
         return true;
     }
 
-    static findByUserName({ username }) {
+    static findByUserName({ username }: { username: string }) {
         const user = UserMemory.find((user) => user.username === username);
         return user;
     }
