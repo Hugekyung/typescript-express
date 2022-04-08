da66f8d57e447b3e45168e76c4651b4add15a1e5
haechan <kiki9510@gmail.com>
2022-04-08 17:30:30 +0900

    Added signup api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,22 @@
 import { Router, Request, Response } from "express";
+import { v4 as uuidV4 } from "uuid";
+import userMemory from "./auth.model";
 
 const AuthRouter = Router();
 
-AuthRouter.post("/signup", (req: Request, res: Response) => {});
+AuthRouter.post("/signup", (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+
+        const userId = uuidV4();
+        userMemory.push({
+            id: userId,
+            username,
+            password,
+        });
+    } catch (error) {
+        throw new Error("Server Error");
+    }
+});
 
 export default AuthRouter;
